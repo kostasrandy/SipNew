dfd0c2043db51fb35bc05dec0cec0d406c74d8d0
gpats <patseas.g@gmail.com>
2017-01-16 20:00:34 +0200

    new version

--- sip-communicator/src/net/java/sip/communicator/gui/AuthenticationSplash.java
+++ sip-communicator/src/net/java/sip/communicator/gui/AuthenticationSplash.java
@@ -55,9 +55,7 @@
  * originally written at the National Center for Supercomputing Applications,
  * University of Illinois, Urbana-Champaign.
  */
-
 package net.java.sip.communicator.gui;
-
 import java.awt.*;
 import java.awt.geom.*;
 import java.awt.event.*;
@@ -67,9 +65,7 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import net.java.sip.communicator.common.Utils;
 import net.java.sip.communicator.common.*;
-
 //import samples.accessory.StringGridBagLayout;
-
 /**
  * Sample login splash screen
  */
@@ -81,43 +77,36 @@ public class AuthenticationSplash
     JTextField userNameTextField = null;
     JLabel     realmValueLabel = null;
     JPasswordField passwordTextField = null;
-
     /**
      * Resource bundle with default locale
      */
     private ResourceBundle resources = null;
-
     /**
      * Path to the image resources
      */
     private String imagePath = null;
-
     /**
      * Command string for a cancel action (e.g., a button).
      * This string is never presented to the user and should
      * not be internationalized.
      */
     private String CMD_CANCEL = "cmd.cancel" /*NOI18N*/;
-
     /**
      * Command string for a help action (e.g., a button).
      * This string is never presented to the user and should
      * not be internationalized.
      */
     private String CMD_HELP = "cmd.help" /*NOI18N*/;
-
     /**
      * Command string for a login action (e.g., a button).
      * This string is never presented to the user and should
      * not be internationalized.
      */
     private String CMD_LOGIN = "cmd.login" /*NOI18N*/;
-
     // Components we need to manipulate after creation
     private JButton loginButton = null;
     private JButton cancelButton = null;
     private JButton helpButton = null;
-
     /**
      * Creates new form AuthenticationSplash
      */
@@ -129,7 +118,6 @@ public class AuthenticationSplash
         pack();
         centerWindow();
     }
-
     /**
      * Loads locale-specific resources: strings, images, et cetera
      */
@@ -138,7 +126,6 @@ public class AuthenticationSplash
         Locale locale = Locale.getDefault();
         imagePath = ".";
     }
-
     /**
      * Centers the window on the screen.
      */
@@ -155,7 +142,6 @@ public class AuthenticationSplash
             this.setLocation(newLocation);
         }
     } // centerWindow()
-
     /**
      *
      * We use dynamic layout managers, so that layout is dynamic and will
@@ -180,12 +166,9 @@ public class AuthenticationSplash
     {
         Container contents = getContentPane();
         contents.setLayout(new BorderLayout());
-
         String title = Utils.getProperty("net.java.sip.communicator.gui.AUTH_WIN_TITLE");
-
         if(title == null)
             title = "Registrar Login";
-
         setTitle(title);
         setResizable(false);
         addWindowListener(new WindowAdapter()
@@ -195,40 +178,29 @@ public class AuthenticationSplash
                 dialogDone(CMD_CANCEL);
             }
         });
-
         // Accessibility -- all frames, dialogs, and applets should
         // have a description
         getAccessibleContext().setAccessibleDescription("Authentication Splash");
-
         String authPromptLabelValue = Utils.getProperty("net.java.sip.communicator.gui.AUTHENTICATION_PROMPT");
-
         if(authPromptLabelValue  == null)
             authPromptLabelValue  = "Please enter user name and password to use when registering!";
-
         JLabel splashLabel = new JLabel(authPromptLabelValue );
         splashLabel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
         splashLabel.setHorizontalAlignment(SwingConstants.CENTER);
         splashLabel.setHorizontalTextPosition(SwingConstants.CENTER);
         contents.add(splashLabel, BorderLayout.NORTH);
-
         JPanel centerPane = new JPanel();
         centerPane.setLayout(new GridBagLayout());
-
         userNameTextField = new JTextField(); // needed below
-
         // user name label
         JLabel userNameLabel = new JLabel();
         userNameLabel.setDisplayedMnemonic('U');
         // setLabelFor() allows the mnemonic to work
         userNameLabel.setLabelFor(userNameTextField);
-
         String userNameLabelValue = Utils.getProperty("net.java.sip.communicator.gui.USER_NAME_LABEL");
-
         if(userNameLabelValue == null)
             userNameLabelValue = "User name";
-
         int gridy = 0;
-
         userNameLabel.setText(userNameLabelValue);
         GridBagConstraints c = new GridBagConstraints();
         c.gridx=0;
@@ -236,7 +208,6 @@ public class AuthenticationSplash
         c.anchor=GridBagConstraints.WEST;
         c.insets=new Insets(12,12,0,0);
         centerPane.add(userNameLabel, c);
-
         // user name text
         c = new GridBagConstraints();
         c.gridx=1;
@@ -245,18 +216,13 @@ public class AuthenticationSplash
         c.weightx=1.0;
         c.insets=new Insets(12,7,0,11);
         centerPane.add(userNameTextField, c);
-
         //username example
         if(GuiManager.isThisSipphoneAnywhere)
         {
-
             String egValue = Utils.getProperty("net.java.sip.communicator.sipphone.USER_NAME_EXAMPLE");
-
             if(egValue == null)
                 egValue = "Example: 1-747-555-1212";
-
             JLabel userNameExampleLabel = new JLabel();
-
             userNameExampleLabel.setText(egValue);
             c = new GridBagConstraints();
             c.gridx=0;
@@ -265,11 +231,8 @@ public class AuthenticationSplash
             c.fill=GridBagConstraints.HORIZONTAL;
             c.insets=new Insets(12,12,0,0);
             centerPane.add(userNameExampleLabel, c);
-
         }
-
         passwordTextField = new JPasswordField(); //needed below
-
         // password label
         JLabel passwordLabel = new JLabel();
         passwordLabel.setDisplayedMnemonic('P');
@@ -281,10 +244,8 @@ public class AuthenticationSplash
         c.gridy = gridy;
         c.anchor = GridBagConstraints.WEST;
         c.insets = new Insets(11, 12, 0, 0);
-
         centerPane.add(
             passwordLabel, c);
-
         // password text
         passwordTextField.setEchoChar('\u2022');
         c = new GridBagConstraints();
@@ -294,20 +255,15 @@ public class AuthenticationSplash
         c.weightx = 1.0;
         c.insets = new Insets(11, 7, 0, 11);
         centerPane.add(passwordTextField, c);
-
         //Set a relevant realm value
         //Bug report by Steven Lass (sltemp at comcast.net)
         //JLabel realmValueLabel = new JLabel("SipPhone.com"); // needed below
-
-
         // realm label
-
         JLabel realmLabel = new JLabel();
         realmLabel.setDisplayedMnemonic('R');
         realmLabel.setLabelFor(realmValueLabel);
         realmLabel.setText("Realm");
         realmValueLabel = new JLabel();
-
         if (!GuiManager.isThisSipphoneAnywhere) {
             c = new GridBagConstraints();
             c.gridx = 0;
@@ -322,7 +278,6 @@ public class AuthenticationSplash
             c.insets = new Insets(11, 7, 0, 11);
             centerPane.add(realmValueLabel, c);
         }
-
         // Buttons along bottom of window
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new BoxLayout(buttonPanel, 0));
@@ -337,10 +292,8 @@ public class AuthenticationSplash
             }
         });
         buttonPanel.add(loginButton);
-
         // space
         buttonPanel.add(Box.createRigidArea(new Dimension(5, 0)));
-
         cancelButton = new JButton();
         cancelButton.setText("Cancel");
         cancelButton.setActionCommand(CMD_CANCEL);
@@ -352,9 +305,7 @@ public class AuthenticationSplash
             }
         });
         buttonPanel.add(cancelButton);
-
         buttonPanel.add(Box.createRigidArea(new Dimension(5, 0)));
-
         helpButton = new JButton();
         helpButton.setMnemonic('H');
         helpButton.setText("Help");
@@ -372,17 +323,12 @@ public class AuthenticationSplash
         c.gridy = 3;
         c.gridwidth = 2;
         c.insets = new Insets(11, 12, 11, 11);
-
         centerPane.add(buttonPanel, c);
-
         contents.add(centerPane, BorderLayout.CENTER);
         getRootPane().setDefaultButton(loginButton);
         equalizeButtonSizes();
-
         setFocusTraversalPolicy(new FocusTraversalPol());
-
     } // initComponents()
-
     /**
      * Sets the buttons along the bottom of the dialog to be the
      * same size. This is done dynamically by setting each button's
@@ -392,16 +338,13 @@ public class AuthenticationSplash
      */
     private void equalizeButtonSizes()
     {
-
         JButton[] buttons = new JButton[] {
             loginButton, cancelButton
         };
-
         String[] labels = new String[buttons.length];
         for (int i = 0; i < labels.length; i++) {
             labels[i] = buttons[i].getText();
         }
-
         // Get the largest width and height
         int i = 0;
         Dimension maxSize = new Dimension(0, 0);
@@ -416,12 +359,10 @@ public class AuthenticationSplash
             maxSize.height =
                 Math.max(maxSize.height, (int) textBounds.getHeight());
         }
-
         Insets insets =
             buttons[0].getBorder().getBorderInsets(buttons[0]);
         maxSize.width += insets.left + insets.right;
         maxSize.height += insets.top + insets.bottom;
-
         // reset preferred and maximum size since BoxLayout takes both
         // into account
         for (i = 0; i < buttons.length; ++i) {
@@ -429,7 +370,6 @@ public class AuthenticationSplash
             buttons[i].setMaximumSize( (Dimension) maxSize.clone());
         }
     } // equalizeButtonSizes()
-
     /**
      * The user has selected an option. Here we close and dispose the dialog.
      * If actionCommand is an ActionEvent, getCommandString() is called,
@@ -465,7 +405,6 @@ public class AuthenticationSplash
         setVisible(false);
         dispose();
     } // dialogDone()
-
     /**
      * This main() is provided for debugging purposes, to display a
      * sample dialog.
@@ -483,7 +422,6 @@ public class AuthenticationSplash
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(false);
-
         AuthenticationSplash dialog = new AuthenticationSplash(frame, true);
         dialog.addWindowListener(new WindowAdapter()
         {
@@ -491,7 +429,6 @@ public class AuthenticationSplash
             {
                 System.exit(0);
             }
-
             public void windowClosed(WindowEvent event)
             {
                 System.exit(0);
@@ -500,7 +437,6 @@ public class AuthenticationSplash
         dialog.pack();
         dialog.setVisible(true);
     } // main()
-
     private class FocusTraversalPol extends LayoutFocusTraversalPolicy
     {
         public Component getDefaultComponent(Container cont)
